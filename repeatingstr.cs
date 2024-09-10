54ad45a51fa2b8e3980b3c2e6de573c45cb5e604
antwi-J <kwakuantwij@gmail.com>
2024-09-10 18:24:40 +0100

    Add files via upload

---
+++ repeatingstr.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repeating
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+         
+           
+
+            
+            string message = "" + name;
+
+            
+            string repeatedMessage = message + " " + message + " " + message + " " + message + " " + message;
+
+            
+            Console.WriteLine(repeatedMessage);
+            Console.Read();
+             
+        }
+    }
+}
