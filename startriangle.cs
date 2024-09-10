bc253583ed37c4a1f2df46398c70041350531108
antwi-J <kwakuantwij@gmail.com>
2024-09-10 18:10:22 +0100

    Add files via upload

---
+++ startriangle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace startriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   *   \n  ***  \n ***** \n*******");
+            Console.Read();
+
+        }
+    }
+}
