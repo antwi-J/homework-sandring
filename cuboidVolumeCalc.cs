c128e76c79991de19667dcb8c7c8e08c755a08f9
antwi-J <kwakuantwij@gmail.com>
2024-09-10 18:35:42 +0100

    Add files via upload

---
+++ cuboidVolumeCalc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cuboidVolumeCalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //length entered here
+            Console.Write("Enter the length of the cuboid: ");
+            double length = double.Parse(Console.ReadLine());
+
+            //width entered here
+            Console.Write("Enter the width of the cuboid: ");
+            double width = double.Parse(Console.ReadLine());
+
+            //height entered here
+            Console.Write("Enter the height of the cuboid: ");
+            double height = double.Parse(Console.ReadLine());
+
+            //calculation
+            double volume = length * width * height;
+
+            //volume print
+            Console.WriteLine("The volume of the cuboid is: " + volume);
+            Console.Read();
+        }
+    }
+}
