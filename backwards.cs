4ada809a6e578584df4eb1989372d7939e849a38
antwi-J <kwakuantwij@gmail.com>
2024-09-10 18:29:20 +0100

    Add files via upload

---
+++ backwards.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backwards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter word 1: ");
+            string word1 = Console.ReadLine();
+
+            Console.Write("Enter word 2: ");
+            string word2 = Console.ReadLine();
+
+            Console.Write("Enter word 3: ");
+            string word3 = Console.ReadLine();
+
+            Console.Write("Enter word 4: ");
+            string word4 = Console.ReadLine();
+
+            Console.Write("Enter word 5: ");
+            string word5 = Console.ReadLine();
+
+            
+            Console.WriteLine(word5);
+            Console.WriteLine(word4);
+            Console.WriteLine(word3);
+            Console.WriteLine(word2);
+            Console.WriteLine(word1);
+            Console.Read();
+        }
+    }
+}
